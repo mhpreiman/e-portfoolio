31e89200f18c4234e5cd298ea35351a99a516c07
mhpreiman <mhpreiman@gmail.com>
2018-02-16 12:53:01 +0200

    Refactor header scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,55 @@
 $(function(){
 // ========== HEADER STYLE SWITCHER ON PAGE CHANGE ========== 
-
-//Create + fill arrays for pages and corresponding headers
-var mypages = [];               //page ID:      $(mypages[1][0]).attr('id')     #home
-var headerNames = [];           //header:       headerNames[3][3]               header-blue
     
-$('body .page').each(function(pageIndex,mypage) {
-    //Create pages list
-    mypages.push($(mypage));
+//Create object for pageIDs (#home) and corresponding headers (header-blue)
+pagesAndHeaders = {};
+    
+$('body .page').each(function(index,mypage) {
 
-    //Create headerNames list
-    obj = {};
-    if (pageIndex == 0) {
-        obj[pageIndex] = "";
-    } else if (pageIndex % 2 != 0) {
-        obj[pageIndex] = "header-blue";
-    } else {
-        obj[pageIndex] = "header-white";
-    }
-    headerNames.push(obj);
+    var pageID  = $(mypage).attr('id') ;
+    var className = '';
+    
+    if (index == 0) 
+        className = "";
+    else if (index % 2 != 0) 
+        className = "header-blue";
+    else 
+        className= "header-white";
+    
+    pagesAndHeaders[pageID] = className;    //save as object's property
+
 });
-    
+
+//Check-set classes    
 function scrollClass() {
     var scrollPosition = $(this).scrollTop(); 
     var header = $('#main-header');
     var headerHeight = header.height();   
-    
+    var pagesAndHeadersObject = Object.keys(pagesAndHeaders);
+//    console.log( pagesAndHeadersObject);
     var headerClassRegex = /(^|\s)header-\S+/g;          //header-   eg header-blue
 
     $('body .page').each(function(pageIndex,mypage) {
-        var pageID = $(mypages[pageIndex][0]).attr('id');       // html id
+        var pageID = pagesAndHeadersObject[pageIndex];          //eg home
+        var headerClass = pagesAndHeaders[pageID]; 
         var pageTopPosition = $('#'+pageID).position().top;
-        var i = 1;
-        var lastPage = 0;
-        if (mypages.length-1 <= pageIndex) {
-            i = 0;
-            lastPage = 100;
-        } 
-        var nextPagePosition = $(mypages[pageIndex+i][0]).position().top+lastPage;
-        var arrayHeader = headerNames[pageIndex][pageIndex]; 
-        
-        //actual (realtime) header class
-        // var headerClassObj = header.attr("class").match(headerClassRegex);
-        // var headerToString = JSON.stringify(headerClassObj);
-        // var headerClass = headerToString.replace(/^\["\s((header-)[\w\d]+)\"\]$/,'$1');
+        var next = 1;
+        if (pagesAndHeadersObject.length-1 <= pageIndex) 
+            next = 0;
+        console.log( pageTopPosition);
+        var nextPagePosition = $('#'+pagesAndHeadersObject[pageIndex+next]).position().top + pageTopPosition;
         
         //If top of scroll (+navigation) is lower than page (eg #home) AND
         //if scroll is located higher than the next page element
         if (scrollPosition + headerHeight >= pageTopPosition && 
            scrollPosition + headerHeight < nextPagePosition) {
-            
-            //remove all "header-" classes
+
+            //Remove all "header-" classes
             header.removeClass(function(index,css) {
                 return (css.match(headerClassRegex) || []).join(' ');
             });
-            header.addClass(arrayHeader);
+            
+            header.addClass(headerClass);
             
             // Also change active page on righthand menu
             $('a').removeClass('active');
